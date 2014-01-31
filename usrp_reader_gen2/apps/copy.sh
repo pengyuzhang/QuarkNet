df19419d48f4fee8e41616eacd79a8a620b3524d
pengyuzhang <zhangelsu@gmail.com>
2014-01-31 13:58:11 -0500

    adding usrp reader gen2 version

---
+++ usrp_reader_gen2/apps/copy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+scp matched_filter.out pyzhang@none.cs.umass.edu:/nfs/none/users4/pyzhang/signal_analysis/
+scp command_gate.out pyzhang@none.cs.umass.edu:/nfs/none/users4/pyzhang/signal_analysis/
+scp mag.out pyzhang@none.cs.umass.edu:/nfs/none/users4/pyzhang/signal_analysis/
+scp center.out pyzhang@none.cs.umass.edu:/nfs/none/users4/pyzhang/signal_analysis/
+scp rx.out pyzhang@none.cs.umass.edu:/nfs/none/users4/pyzhang/signal_analysis/
+scp mm.out pyzhang@none.cs.umass.edu:/nfs/none/users4/pyzhang/signal_analysis/
---
+++ usrp_reader_gen2/apps/run_wisp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo GR_SCHEDULER=STS nice -n -20 ./WISP_reader.py
